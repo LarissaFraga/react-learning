7d52a9247dd1836285d5188959bd39bc655c4de7
Larissa <larissa.fraga@ufms.br>
2022-02-04 13:01:08 -0400

    Finishing configurations

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,10 +4,7 @@ module.exports = {
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-actions",
-    "@storybook/addon-knobs/register"
+    "@storybook/addon-essentials"
   ],
   "framework": "@storybook/react"
 }
